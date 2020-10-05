d948b7710030c5af9b05a811007c79c099940e5c
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-05 18:24:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,18 @@
 def addition(num1, num2)
-  sum = num1 + num2
-  sum
+  num3 = num1 + num2
+  num3
 end
 
 def subtraction(num1, num2)
-
+  num3 = num1 - num2
 end
 
 def division(num1, num2)
-
+  num3 = num1/num2
 end
 
 def multiplication(num1, num2)
-
+  num3 = num1 * num2
 end
 
 def modulo(num1, num2)
@@ -20,5 +20,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
